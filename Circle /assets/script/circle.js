4e338f1ee18a0ee82cfe5379b8e6ee7c0248c55f
analupton <145708917+analupton@users.noreply.github.com>
2024-02-04 04:54:57 +0000

    added js

--- Circle /assets/script/circle.js
+++ Circle /assets/script/circle.js
@@ -1,3 +1,38 @@
-$( "#CircleForm" ).validate({
+$("#CircleForm").validate();
 
-});
+function displayCircleCalculations() {
+    if ($("#CircleForm").valid()) {
+        document.getElementById("diameter").innerHTML = "";
+        document.getElementById("area").innerHTML = "";
+        document.getElementById("circumference").innerHTML = "";
+
+        var radius = parseFloat(document.getElementById("radius").value);
+        var diameter = calcDiameter(radius);
+        var area = calcArea(radius);
+        var circumference = calcCircumference(radius);
+
+        document.getElementById("diameter").innerHTML = diameter.toString();
+        document.getElementById("area").innerHTML = area.toString();
+        document.getElementById("circumference").innerHTML = circumference.toString();
+    }
+}
+
+function calcDiameter(radius) {
+    return 2 * radius;
+}
+
+function calcArea(radius) {
+    return Math.PI * Math.pow(radius, 2);
+}
+
+function calcCircumference(radius) {
+    return 2 * Math.PI * radius; 
+
+}
+
+function clearCircleForm() {
+    document.getElementById("radius").value = "";
+    document.getElementById("radiusError").innerHTML = "";
+    document.getElementById("diameter").innerHTML = "";
+    document.getElementById("area").innerHTML = "";
+    document.getElementsById("circumference").innerHTML = "";
