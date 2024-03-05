543dc05adc729ecb1c75d0627f6b2ae59afd808e
analupton <145708917+analupton@users.noreply.github.com>
2024-03-05 18:23:19 +0000

    fix

--- currency value history/assets/script/chart.js
+++ currency value history/assets/script/chart.js
@@ -1,20 +1,84 @@
+async function Results() {
+    "use strict"
+       
+   
+   var form = $("#CVHform");
+    
+    form.validate(); 
+    
+     if (form.valid()) {
 
+        
+        var BaseCurrency = document.getElementById("basecurrency").value;
+        var ToCurrency = document.getElementById("convertcurrency").value;
+        var FromDate = document.getElementById("FromDate").value;
+        var ToDate = document.getElementById("ToDate").value;
+
+        var apiKey = "NWUV7nTLbvIAdK6Z5W37ooGaSLBDzqYp"
+        
+  
+        var myURL2 = "https://api.polygon.io/v2/aggs/ticker/C:" 
+        + BaseCurrency + ToCurrency + "/range/1/day/" + FromDate + "/" + ToDate + "?adjusted=true&sort=asc&limit=32&apiKey=" + apiKey;
+           var msg2Object = await fetch(myURL2);
+    
+            if (msg2Object.status >= 200 && msg2Object.status <= 299) {            
+                var msg2JSONText = await msg2Object.text();
+           
+                var msg2 = JSON.parse(msg2JSONText);
+               
+        
+                
+            var date = [];
+            var value = [];
+            var numdays = msg2.results.length;
+            if (numdays > 0) {
+              for (var i = 0; i < numdays; i++) {
+                
+                 value[i] = msg2.results[i].c;
+                var tempdate = new Date(msg2.results[i].t);
+                date[i] = tempdate.toLocaleDateString(); 
+            }
+        }
+               
+                var ctx0 = document.getElementById("chartjs-0");
+                var myChart = new Chart(ctx0, {
+                    "type":"line",
+                    "data": {
+                        "labels": date, 
+                        "datasets":[{"label": "Currency Value History",
+                        "data": value,
+                        "fill":false,
+                        "borderColor":"rgb(75, 192, 192)",
+                        "lineTension":0.1}]},
+                          "options":{ 
+                             responsive: false,
+                             maintainAspectRatio: true,
+                            }
+                        }
+                );
+          }  
+          else {
+                alert("Currency Not Found - Status: " + msg2Object.status)
+                return
+            }
+        }
+    }
 
 function ClearForm() {
+
     document.getElementById("basecurrency").value = "";
     document.getElementById("convertcurrency").value = "";
     document.getElementById("FromDate").value = "";
     document.getElementById("ToDate").value = "";
-    document.getElementById("ceo").innerHTML = "";
-    document.getElementById("url").innerHTML = "";
-    document.getElementById("url").href = "";
-    document.getElementById("logo").src = "";
+   
+    document.getElementById("ToDateMsg").innerHTML = "";
+    document.getElementById("FromDateMsg").innerHTML = "";
+
+document.getElementById("BaseCurrency-error").innerHTML = "";
+document.getElementById("ToCurrency-error").innerHTML = "";
     
-    /* Ugly Code to Erase Canvas */
     var canvas0 = document.getElementById("chartjs-0");
     var context0 = canvas0.getContext('2d');    
-    context0.clearRect(0, 0, canvas0.width, canvas0.height);
-    var canvas1 = document.getElementById("chartjs-1");
-    var context1 = canvas1.getContext('2d');    
-    context1.clearRect(0, 0, canvas1.width, canvas1.height);
+    context0.ClearRect(0, 0, canvas0.width, canvas0.height);
+
 }
