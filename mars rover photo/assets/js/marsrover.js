281c58f429c80485f000b60d0f515851cca1070d
analupton <145708917+analupton@users.noreply.github.com>
2024-03-11 01:58:13 +0000

    finally fixed

--- mars rover photo/assets/js/marsrover.js
+++ mars rover photo/assets/js/marsrover.js
@@ -1,14 +1,14 @@
 function myFunction() {
-    var date;
-    if (document.getElementById("Curiosity").checked) {
-        document.getElementById("date").value="2012-08-06";
+    var picturedate;
+    if (document.getElementById("curiosity").checked) {
+        document.getElementById("picturedate").value="2012-08-06";
     }
   
-    if (document.getElementById("Opportunity").checked) {
-        document.getElementById("date").value= "2004-01-26";
+    if (document.getElementById("opportunity").checked) {
+        document.getElementById("picturedate").value= "2004-01-26";
     }
-    if (document.getElementById("Spirit").checked) {
-        document.getElementById("date").value= "2004-01-05";
+    if (document.getElementById("spirit").checked) {
+        document.getElementById("picturedate").value= "2004-01-05";
     }
   }
     
@@ -23,21 +23,21 @@ function myFunction() {
     if (form.valid()) {
             
       var rover;
-      if (document.getElementById("Curiosity").checked) {
-          rover = document.getElementById("Curiosity").value;
+      if (document.getElementById("curiosity").checked) {
+          rover = document.getElementById("curiosity").value;
       }
   
-      if (document.getElementById("Opportunity").checked) {
-          rover = document.getElementById("Opportunity").value;
+      if (document.getElementById("opportunity").checked) {
+          rover = document.getElementById("opportunity").value;
       }
-      if (document.getElementById("Spirit").checked) {
-          rover = document.getElementById("Spirit").value;
+      if (document.getElementById("spirit").checked) {
+          rover = document.getElementById("spirit").value;
       }
    
-      var date = document.getElementById("date").value;
+      var picturedate = document.getElementById("picturedate").value;
       var apiKey = "jeOjEno8SCYQny38H1a8fE7UBhfmOuHPkVh4qF9Y";
     
-          var myURL2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=" + date + "&api_key=" + apiKey;
+          var myURL2 = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=" + picturedate + "&api_key=" + apiKey;
             
           let PictureObject = await fetch(myURL2);
           let Result = await PictureObject.text();
